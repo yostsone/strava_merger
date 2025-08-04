d11e774ff263fdbd4804e5c967b5aa32cd7288b7
katrina <jostsone.k@gmail.com>
2025-08-04 18:00:05 +0300

    MyAccount page

--- src/components/MyAccount/MyAccount.tsx
+++ src/components/MyAccount/MyAccount.tsx
@@ -1,64 +1,77 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from "react-router";
-import { Link } from "react-router-dom";
-import getUserToken from './MyAccountApi';
+import { useLocation, NavLink } from 'react-router';
+import { AxiosResponse } from 'axios';
 import { Profile } from './MyAccountType'
-import { AxiosResponse } from "axios";
-
-/**
- * Set state value from local storage if available
- */
-const initProfileData = ():Profile => {
-  const customerDataValue = localStorage.getItem('customerData');
-
-  return (customerDataValue !== null) ? JSON.parse(customerDataValue) : {
-    firstname: '',
-    lastname: '',
-    photo: '',
-    accessToken: ''
-  };
-};
+import { getUserData } from './MyAccountApi';
+import MyAccountInfo from '../MyAccountInfo/MyAccountInfo';
+import Loader from '../Loader/Loader';
+import { getProfileData, getUserLoginData, useProfileStore } from '../../utils/User';
+import { deleteStorageData } from '../../utils/BrowserDatabase/BrowserDatabase';
+import { USER_LOGIN, AUTH_CODE } from '../../constants';
 
 export default function MyAccount() {
-  const [profileData, setProfileData] = useState <Profile>(initProfileData);
-  const [isLoading, setIsLoading ] = useState<boolean>(true);
+  const [profileData, setProfileData] = useState <Profile>(getProfileData);
+  const [isError, setIsError ] = useState<boolean>(false);
+  // because react in dev mode loads twice, but request with given code is valid only once
+  const [isStrictCheck, setStrictCheck ] = useState<boolean>(false);
   let params = useLocation();
 
+  const [, setProfile ] = useProfileStore();
   useEffect(() => {
+    const userLogin = getUserLoginData();
     let paramsArray = new URLSearchParams(params.search);
-    const clientId = '62468';
-    const client_secret= 'f6d8f544e646f8ad2e9348c431ef81c322cb31ec';
+    const clientId = userLogin.clientId;
+    const clientSecret = userLogin.clientSecret;
 
-    if (profileData.accessToken === '') {
-      getUserToken(clientId, client_secret, paramsArray.get('code'))
-          .then(function (response: AxiosResponse) {
-            const { data: { accessToken, athlete: { firstname, lastname, profile } } } = response;
-            setProfileData({ firstname, lastname, photo: profile, accessToken });
-            window.localStorage.setItem('customerData', JSON.stringify({ firstname, lastname, photo: profile, accessToken }));
-            setIsLoading(false);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
+    if (!profileData.accessToken) {
+      getUserData(clientId, clientSecret, paramsArray.get(AUTH_CODE))
+        .then((response: AxiosResponse) => {
+          const {
+            data: {
+              access_token,
+              athlete: {
+                id, firstname, lastname, username, profile, created_at, bio, city, weight
+              }
+            }
+          } = response;
+
+          const athlete = { id, firstname, lastname, username, photo: profile, createdAt: created_at, bio, city, weight, accessToken: access_token } ;
+          setProfile(JSON.stringify(athlete));
+          setProfileData(athlete);
+          setStrictCheck(true);
+        })
+        .catch(() => {
+          if (!isStrictCheck) {
+            setIsError(true);
+          }
+        })
+        .finally(() => {
+          deleteStorageData(USER_LOGIN);
+        });
     }
-  }, [params.search, profileData.accessToken]);
+  }, [isStrictCheck, params.search, profileData.accessToken, setProfile]);
 
-  if (isLoading && profileData.accessToken === '') {
-    return <h2>🌀 Loading...</h2>;
+  const { firstname, lastname, photo } = profileData;
+
+  if (firstname && lastname && photo ) {
+    return (
+        <div className="flex flex-col p-10">
+          <MyAccountInfo props={ profileData } />
+          <div className="flex justify-between pt-10">
+            <NavLink to={'/stats'} className="lowercase text-indigo-600 border-indigo-600 border-4 p-2 font-bold">
+              Check My Stats
+            </NavLink>
+            <NavLink to={'/stats'} className="lowercase text-indigo-600 border-indigo-600 border-4 p-2 font-bold">
+              Merge Activities
+            </NavLink>
+          </div>
+        </div>
+    );
   }
 
-  return (
-    <div className="flex flex-col p-20">
-      <div className="flex justify-center">
-          <h1 className="text-3xl">Hello { profileData.firstname?? profileData.firstname } { profileData.lastname?? profileData.lastname }! This is your acccount!</h1>
-      </div>
-      <div className="flex justify-center">
-        <img className="size-24 rounded-full border-4 border-indigo-500" src={ profileData.photo ?? profileData.photo } alt="Profile"/>
-      </div>
-      <div className="flex justify-between">
-        <Link to={'/stats'}>Check My Stats</Link>
-        <Link to={'/stats'}>Merge Activities</Link>
-      </div>
-    </div>
-  );
+  if (isError && !isStrictCheck) {
+    window.location.reload();
+  }
+
+  return <Loader />
 }
