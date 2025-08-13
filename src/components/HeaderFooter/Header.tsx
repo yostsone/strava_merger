62947f3ed38a9ef035f5436db57cdf9109d065b1
katrina <jostsone.k@gmail.com>
2025-08-13 16:25:30 +0300

    Added redirect to logout, minor style changes

--- src/components/HeaderFooter/Header.tsx
+++ src/components/HeaderFooter/Header.tsx
@@ -4,9 +4,9 @@ import { deleteStorageData } from '../../utils/BrowserDatabase';
 import { PROFILE_DATA , PROFILE_DETAILS } from '../../constants';
 import HeaderLogo from './HeaderLogo';
 
-const logOut = ():void => {
+const logOut = () => {
+  window.location.href = '/';
   deleteStorageData(PROFILE_DATA);
-  window.location.reload();
 };
 
 export default function Header() {
@@ -30,11 +30,11 @@ export default function Header() {
   return (
       <div className="flex justify-between bg-indigo-500 h-14">
         <div className="flex justify-start basis-1/3">
-          <button className="p-2" onClick={ logOut }>Log out</button>
+          <button className="p-2 text-emerald-200" onClick={ logOut }>Log out</button>
         </div>
         <HeaderLogo />
         <div className="flex justify-end basis-1/3">
-          <span className="p-4 border-transparent">{ username }</span>
+          <span className="p-4 border-transparent text-emerald-200">{ username }</span>
           <img className="m-2 size-10 rounded-full border-4 border-indigo-500" src={ photo } alt="Profile"/>
         </div>
       </div>
